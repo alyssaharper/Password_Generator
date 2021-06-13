d0ec9c512f4bd5762d2195b4a270f6b73d0fc4a7
Alyssa harper <alyssaharper@Alyssas-Air.home>
2021-06-13 13:07:43 -0400

    Created new variables, deleted extra functions, made an arrry to filter out false statements

--- Develop/script.js
+++ Develop/script.js
@@ -5,21 +5,21 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   function generatePassword() {
-    // var lengthPrompt = window.prompt("How long do you want your password? Please choose between 8 and 128");
-    // if (lengthPrompt >= 8 && lengthPrompt <= 128) {
-    //   var numberRequest = window.confirm("Do you want your password to include numbers? Please click 'OK' for yes or 'Cancel' for no.");
-    // } else {
-    //   window.alert("Please choose a valid number between 8 and 128");
-    // }
-    // if (numberRequest === true || numberRequest === false) {
-    //   var lowercaseRequest = window.confirm("Do you want your password to include lowercase letters? Please click 'OK' for yes or 'Cancel' for no.");
-    // } 
-    // if (lowercaseRequest === true || lowercaseRequest === false) {
-    //   var uppercaseRequest = window.confirm("Do you want your password to include uppercase letters? Please click 'OK' for yes or 'Cancel' for no.");
-    // }
-    // if (uppercaseRequest === true || uppercaseRequest === false) {
-    //   var symbolRequest = window.confirm("Do you want your password to include symbols? Please click 'OK' for yes or 'Cancel' for no.");
-    // }
+    var lengthPrompt = window.prompt("How long do you want your password? Please choose between 8 and 128");
+    if (lengthPrompt >= 8 && lengthPrompt <= 128) {
+      var numberRequest = window.confirm("Do you want your password to include numbers? Please click 'OK' for yes or 'Cancel' for no.");
+    } else {
+      window.alert("Please choose a valid number between 8 and 128");
+    }
+    if (numberRequest === true || numberRequest === false) {
+      var lowercaseRequest = window.confirm("Do you want your password to include lowercase letters? Please click 'OK' for yes or 'Cancel' for no.");
+    } 
+    if (lowercaseRequest === true || lowercaseRequest === false) {
+      var uppercaseRequest = window.confirm("Do you want your password to include uppercase letters? Please click 'OK' for yes or 'Cancel' for no.");
+    }
+    if (uppercaseRequest === true || uppercaseRequest === false) {
+      var symbolRequest = window.confirm("Do you want your password to include symbols? Please click 'OK' for yes or 'Cancel' for no.");
+    }
 
     function getRandomLowercase() {
         return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -41,35 +41,24 @@ function writePassword() {
         symbol: getRandomSymbol
     };
 
-    var generatedArr = [{ lower }, { upper }, { number }, { symbol }];
-    console.log(generatedArr);
+    var typesCount = numberRequest + lowercaseRequest + uppercaseRequest + symbolRequest;
 
-    function wantNumbers() {
-        if (numberRequest === true) {
-         getRandomLowercase();
-        }
+        // console.log(typesCount)
+
+    var generatedArr = [{ numberRequest }, { lowercaseRequest }, { uppercaseRequest
+     }, { symbolRequest }].filter(item => Object.values(item)[0]);
+        // console.log(generatedArr);
+    
+    if(typesCount === 0) {
+        return window.alert("You must select at least character type.")
     }
-    function wantLowercase() {
-        if (lowercaseRequest === true) {
-            getRandomLowercase();
-        }
-    }
-    function wantUppercase() {
-        if (uppercaseRequest === true) {
-            getRandomUppercase();
-        }
-    }
-    function wantSymbols() {
-        if (symbolRequest === true) {
-            getRandomSymbol();
-        }
-    }
+
 
   }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-33
+
 }
 
 // Add event listener to generate button
